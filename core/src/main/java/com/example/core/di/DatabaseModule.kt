b5d7b20414ffa714fc00702a303d43b08ba214cc
Yudha Haris Purnama <yudha@clade.ventures>
2023-08-03 13:06:03 +0700

    feat: add sql chiper

--- core/src/main/java/com/example/core/di/DatabaseModule.kt
+++ core/src/main/java/com/example/core/di/DatabaseModule.kt
@@ -9,18 +9,25 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 class DatabaseModule {
 
+    val passphrase: ByteArray = SQLiteDatabase.getBytes("github".toCharArray())
+    val factory = SupportFactory(passphrase)
+
     @Singleton
     @Provides
     fun provideDatabase(@ApplicationContext context: Context): GithubUserDatabase = Room.databaseBuilder(
         context,
         GithubUserDatabase::class.java, "GithubUser.db"
-    ).fallbackToDestructiveMigration().build()
+    ).fallbackToDestructiveMigration()
+        .openHelperFactory(factory)
+        .build()
 
     @Provides
     fun provideGithubUserDao(database: GithubUserDatabase): GithubUserDao = database.githubUserDao()
